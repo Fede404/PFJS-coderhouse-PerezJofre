c8094d9c06f3dd9111c5c100d572de640e361a20
fedeperezj <federicoperezj@gmail.com>
2024-05-31 13:32:43 -0300

    5 - Finalizada sección lista de productos y ajustes de código

---
+++ js/script.js
@@ -0,0 +1,24 @@
+/* 
+    LISTA DE VINOS A UTILIZAR
+        TINTOS
+            1 - Trivento Reserve - Cabernet Sauvignon - 2021 - $5714.00
+            2 - Casillero del Diablo - Cabernet Sauvignon - 2021 - $6094.00
+            3 - Altos las Hormigas - Malbec - 2021 - $6460.00
+            4 - Tinto Negro Uco Valley - Malbec - 2022 - $8245.00
+            5 - Lo Red - Blend - 2020 - $15300.00
+            6    Bira Rosso D'Uco - Blend - 2022 - $13600.00
+        BLANCOS
+            1 - Trivento Golden Reserve - Chardonnay - 2019 - $12618.00
+            2 - Zuccardi Q - Chardonnay - 2021 - $17422.00
+            3 - Cono Sur Reserva Especial - Sauvignon Blanc - 2021 - $15565.00
+            4 - Zuccardi Polígonos Tupungato - Sauvignon Blanc - 2023 - $22182.00
+            5 - Trivento White - Malbec - 2022 - $5714.00
+            6 - Angelica Zapata - Chardonnay - 2020 - $25257.00
+        ROSADOS
+            1 - Flora by Zaha - Rose - Pinot Noir - 2022 - $15130.00
+            2 - Martir - Rose - 2023 - $22525.00
+            3 - Saint Felicien - Rose - 2023 - $26507.00
+            4 - Nacha - Rose - Pinot Noir - 2022 - $32725.00
+            5 - Bira Rosa di Rosso - Blend - 2023 - $13600.00
+            6 - Trivento Reserve - Rose - Blend - $5714.00
+*/
